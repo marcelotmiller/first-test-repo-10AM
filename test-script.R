7ca7354d2542d1a0c7026ff03fe181db87100978
marcelotmiller <marcelotmiller@gmail.com>
2025-05-16 10:15:21 -0700

    changing colors

--- test-script.R
+++ test-script.R
@@ -3,6 +3,9 @@ library(lterdatasampler)
 
 ggplot(data = pie_crab,
 aes(x = latitude,
-    y = size)) + 
+    y = size,
+    color = latitude)) + 
   geom_point()
+scale_color_gradient(low = "#56B1F7",
+                     high = "#33B")
 
